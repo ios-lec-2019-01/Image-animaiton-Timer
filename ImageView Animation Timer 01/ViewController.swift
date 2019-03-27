74dc27d84bbb73c4952a816a3d52f5a2a9f1ddf6
dit08 <dit@imac-3.local>
2019-03-27 16:31:16 +0900

    이미지 타이머 Play Pause Stop

--- ImageView Animation Timer 01/ViewController.swift
+++ ImageView Animation Timer 01/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var alienImageView: UIImageView!
+    @IBOutlet weak var countlavel: UILabel!
+    var count = 1
+    var isAnimation = false
+    var myTimer = Timer()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        alienImageView.image = UIImage(named: "Image1")
     }
-
-
+    @IBAction func playBtnPressed(_ sender: Any) {
+    print("play pressed")
+    //Timer 호출
+        myTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(doAnimation), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func PauseBtnPressed(_ sender: Any) {
+        print("Pause pressed")
+        myTimer.invalidate()
+    }
+    @IBAction func StopbtnPressed(_ sender: Any) {
+        print("stop pressed")
+        myTimer.invalidate()
+        count = 0
+        countlavel.text = String(count)
+    }
+    @objc func doAnimation (){
+        if count == 5{
+            count = 1
+        }else {
+            count+=1
+        }
+        alienImageView.image = UIImage(named: "Image\(count)")
+        countlavel.text = String(count)
+    }
 }
 
